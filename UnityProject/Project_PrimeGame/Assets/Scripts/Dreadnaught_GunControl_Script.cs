cdf1d98845488607473ec2a3034b8872382b59a2
Roy Wilmer <r.wilmer@live.nl>
2021-02-06 17:10:53 +0100

    Added Health UI for player Dreadnaught

--- UnityProject/Project_PrimeGame/Assets/Scripts/Dreadnaught_GunControl_Script.cs
+++ UnityProject/Project_PrimeGame/Assets/Scripts/Dreadnaught_GunControl_Script.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class Dreadnaught_GunControl_Script : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] guns = new GameObject[3];
+    private int activeGun = 0;
+
+    private void Start()
     {
-        
+        Deactivate();
+        CycleGun();
+    }
+
+    //call this before switching guns to deactivate all of them
+    private void Deactivate()
+    {
+        foreach (GameObject gun in guns)
+        {
+            gun.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Activate Gun1")) 
+        {
+            Deactivate();
+            guns[0].SetActive(true);
+        }
+        if (Input.GetButtonDown("Activate Gun2")) 
+        {
+            Deactivate();
+            guns[1].SetActive(true);
+        }
+        if (Input.GetButtonDown("Activate Gun3")) 
+        {
+            Deactivate();
+            guns[2].SetActive(true);
+        }
+        if (Input.GetButtonDown("Next Gun"))
+        {
+            Deactivate();
+            activeGun++;
+            CycleGun();
+        }
+        if (Input.GetButtonDown("Prev Gun"))
+        {
+            Deactivate();
+            activeGun--;
+            CycleGun();  
+        }
+    }
+
+    
+    private void CycleGun() 
+    {
+        int gunAmount = guns.Length;
+        activeGun = activeGun % gunAmount;
+        if (activeGun < 0)
+        {
+            activeGun += gunAmount;
+        }
+        guns[activeGun].SetActive(true);
     }
 }
---
+++ UnityProject/Project_PrimeGame/Assets/Scripts/UI_Health.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Health : MonoBehaviour
+{
+    [Tooltip("Number of trailers including the first \'locomotive\' section")]
+    public GameObject[] trailers = new GameObject[4];
+    public Text healthText;
+    public float[] trailerHealth;
+
+    void Start()
+    {
+        trailerHealth = new float[trailers.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetHealth();
+        string healthTextTemp = "";
+        for (int i = 0; i < trailerHealth.Length; i++) 
+        {
+            healthTextTemp += string.Format("Trailer {0}: {1} \n", i+1, trailerHealth[i]);
+            //healthTextTemp += "Trailer " + i + ": "+ trailerHealth[i] + "\n";
+        }
+        healthText.text = healthTextTemp;
+        
+        
+        /*if (trailerHealth.Length == 1)
+        {
+            healthText.text = "Locomotive Health: " + trailerHealth[0];
+        }
+        if (trailerHealth.Length == 2)
+        {
+            healthText.text = "Locomotive Health: " + trailerHealth[0] + "\n Trailer Health: " + trailerHealth[1];
+        }
+        if (trailerHealth.Length == 3)
+        {
+            healthText.text = "Locomotive Health: " + trailerHealth[0] + "\n Trailer Health: " + trailerHealth[1] + "\n Trailer Health: " + trailerHealth[2];
+        }
+        if (trailerHealth.Length == 4)
+        {
+            healthText.text = "Locomotive Health: " + trailerHealth[0] + "\n Trailer Health: " + trailerHealth[1] + "\n Trailer Health: " + trailerHealth[2] + "\n Trailer Health: " + trailerHealth[3];
+        }*/
+        
+    }
+
+    private void GetHealth() 
+    {
+        int currentTrailer = 0;
+        foreach (GameObject trailer in trailers)
+        {
+            trailerHealth[currentTrailer] = trailer.GetComponent<Dreadnaught_Health_Script>().health;
+            currentTrailer++;
+        }
+
+    }
+}
